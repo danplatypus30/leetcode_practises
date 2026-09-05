2e0c809ea7dfd916438b3fcf17a2f50faeaac62f
danplatypus30 <daniel.gohch@hotmail.com>
2026-09-06 01:42:03 +0800

    Added better solution for longest common prefix

--- longest_common_prefix.py
+++ longest_common_prefix.py
@@ -30,5 +30,39 @@ class Solution:
         return prefix
         
 
+# faster solution
+class Solution:
+    def longestCommonPrefix2(self, strs: List[str]) -> str:
+        ans = ""
+        # sort lexicographically
+        # flavour
+        # flow
+        # flower
 
+        # apple
+        # banana
+        # curry
+
+        strs = sorted(strs)
+        # just care about first and last since sorted
+        first = strs[0]
+        # last index is arraylen -1
+        last = strs[-1]
+        # if strs = [""], len(first) and len(last) = 0
+        # skip for loop
+        # output ans gracefully
+        # lesser of lengths of first, last
+        for i in range(min(len(first), len(last))):
+            # if char not match, return 
+            if (first[i] != last[i]):
+                return ans
+            # if match, add to ans
+            ans += first[i]
+
+        return ans
+        
+        
+
+
+        
         
